4670a53dec3ad42750b971cdefbb6594093921bc
zhaosj_1991 <zhaosj_1991@163.com>
2019-05-19 03:55:53 -0700

    上传最新的udp和tcp测试程序。

--- udp_test.py
+++ udp_test.py
@@ -4,12 +4,12 @@ import argparse, socket
 from datetime import datetime
 
 MAX_BYTES = 65535
-SERVER_ADDRESS = '192.168.42.35'
+SERVER_ADDRESS = '127.0.0.1'
 
 
-def server(port):
+def server(address=SERVER_ADDRESS, port=4000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((SERVER_ADDRESS, port))
+    sock.bind((address, port))
     print('Listening at {}'.format(sock.getsockname()))
     while True:
         data, address = sock.recvfrom(MAX_BYTES)
@@ -20,11 +20,12 @@ def server(port):
         sock.sendto(data, address)
 
 
-def client(port):
+def client(address=SERVER_ADDRESS, port=4000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.bind((address, port))
     text = 'The time is {}'.format(datetime.now())
     data = text.encode('ascii')
-    sock.sendto(data, (SERVER_ADDRESS, port))
+    sock.sendto(data, (address, port))
     print('The OS assigned me the address {}'.format(sock.getsockname()))
     data, address = sock.recvfrom(MAX_BYTES)
     text = data.decode('ascii')
@@ -34,9 +35,11 @@ if __name__ == '__main__':
     choices = {'client': client, 'server': server}
     parser = argparse.ArgumentParser(description='Send or receive UDP')
     parser.add_argument('role', choices=choices, help='which role to play')
-    parser.add_argument('-p', metavar='PORT', type=int, default=1060, help='UDP port (default 1060)')
+    parser.add_argument('-a', metavar='ADDR', default=SERVER_ADDRESS, help='UDP ADDR (default {})'.format(SERVER_ADDRESS))
+    parser.add_argument('-p', metavar='PORT', type=int, default=4000, help='UDP port (default 4000)')
     args = parser.parse_args()
     function = choices[args.role]
-    function(args.p)
+    print(args.a)
+    function(args.a, args.p)
 
 
